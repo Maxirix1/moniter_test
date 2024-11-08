9a22d43ce5c876cd7b277daa0668525e34c32252
Maxirix <166589012+Maxirix1@users.noreply.github.com>
2024-11-08 09:50:12 +0700

    Update updateStatusHome.php

--- moniter/client/updateStatusHome.php
+++ moniter/client/updateStatusHome.php
@@ -6,8 +6,12 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
         $status_call = $_POST['status_call'];
         $id = $_POST['id'];
 
-        $sql = "UPDATE visit_info SET status_call = :status_call WHERE id = :id AND visit_date = '31102567'";
+    date_default_timezone_set('Asia/Bangkok');
+    $currentDate = date('dmY');
+    $dateTH = date('d') . date('m') . (date('Y') + 543);
+        $sql = "UPDATE visit_info SET status_call = :status_call WHERE id = :id AND visit_date = :dateTH";
         $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':dateTH', $dateTH);
         $stmt->bindParam(':status_call' , $status_call);
         $stmt->bindParam(':id' , $id);
 
